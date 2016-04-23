c0f19048d8927beed5bdc32b4799c4f8b5cd09d4
dchiamp <derek.chiampas@gmail.com>
2016-04-23 15:19:36 -0700

    fuck jsonexporter, switching to json2csv

--- dist/app/controllers/SaveController.js
+++ dist/app/controllers/SaveController.js
@@ -36,16 +36,88 @@ function SaveController ($scope, $http, FileSaver, Blob) {
                     price: 50
                   }]
 
+  var csvTemplate = [ 
+                    {
+                      VariantWeightUnit: "oz",
+                      VariantImage: "URL",
+                      GoogleShoppingCustomLabel4: "",
+                      GoogleShoppingCustomLabel3: "",
+                      GoogleShoppingCustomLabel2: "",
+                      GoogleShoppingCustomLabel1: "",
+                      GoogleShoppingCustomLabel0: "",
+                      GoogleShoppingCustomProduct: "",
+                      GoogleShoppingCondition: "",
+                      GoogleShoppingAdWordsLabels: "",
+                      GoogleShoppingAdWordsGrouping: "",
+                      Description: "",
+                      SEOTitle: "",
+                      GoogleShoppingGoogleProductCategory: "",
+                      GoogleShoppingGender: "",
+                      GoogleShoppingMPN: "",
+                      GiftCard: "FALSE",
+                      ImageSrc: "URL",
+                      TaxableVariantBarcode: "null",
+                      ShippingVariant: "TRUE",
+                      VariantRequires: "TRUE",
+                      VariantCompareAtPrice: "null",
+                      VariantPrice: "Number",
+                      VariantFulfillmentService: "manual",
+                      VariantInventoryPolicy: "deny",
+                      VariantInventoryQty: "1",
+                      VariantInventoryTracker: "null",
+                      VariantGrams: "Number",
+                      VariantSKU: "FULL SKU (generate from input)",
+                      Option3Value: "null",
+                      Option3Name: "null",
+                      Option2Value: "size Value",
+                      Option2Name: "Size",
+                      Option1Value: "color Name",
+                      Option1Name: "Color",
+                      Published: "Boolean",
+                      Tags: "comma seperated array",
+                      Type: "product type",
+                      Vendor: "site name",
+                      Body: "html name",
+                      Title: "design name",
+                      Handle: "SKU_Name"
+                    }
+                    ]
+
+    var objOfObjs = {
+                      firstNames: {Handle: "Title", handle1: "sku-product-1"},
+                      lastNames: { handle1: "name string 1", person2: "Paterson" }
+                    }
+    var try1 =  [
+                    {
+                      Body: "html name",
+                      Title: "design name",
+                      Handle: "SKU_Name"
+                    }
+                  ]
+
+    var try3 =  [
+                {
+                  Body: "html name",
+                  Title: "design name",
+                  Handle: {SKU_Name_1: "skuName1", SKU_Name_2: "skuName2"}
+                }
+              ]
+
+
+var arrayOfArrays = [ ["Joe", "jon", "jack"], ["Sue", "sam", "sal" ] ]
+
   $scope.newProduct = {}
 
     // put product model in service?
     function convertAndDowload () {
-      console.log("product obj to be converted: ", product)
+      console.log("product obj to be converted: ", try3)
       // $http
       //   .post('/api/convert', $scope.product)
       //   .then(function(response){
-        $http.post('/api/convert', product).then(function(response) {
-          console.log(response.data)
+      $http
+        .post('/api/convert', try3)
+        .then(function(response) {
+          console.log("CSV from server", response.data)
           var productCSV = response.data
          // call download function
           vm.download(productCSV)
