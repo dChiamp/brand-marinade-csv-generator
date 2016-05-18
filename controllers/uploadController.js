ea8bb674e7641fef4bdef028a99fdf6b32bbb8ee
dchiamp <derek.chiampas@gmail.com>
2016-05-17 17:35:38 -0700

    adds images before replacing them with live images

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -6,17 +6,8 @@ var request = require('request');
 
 var apiPostUrl = "https://9852e9327df153b6303e9d74c09077b4:78f1c269ac2e9642240f588bec8548fd@printing-site.myshopify.com/admin/products.json"
 
-var testProductPostOld = {
-                      "product": {
-                        "title": "test3",
-                        "body_html": "<strong>Good test3!<\/strong>",
-                        "vendor": "biz3",
-                        "product_type": "thrashplank3",
-                        "published": false
-                      }
-                    }
-
-var masterArray  = [ { 'Variant SKU': 'json-upload-hoodie-heather-sm',
+var masterArray  = [ 
+    { 'Variant SKU': 'json-upload-hoodie-heather-sm',
       'Variant Image': 'http://productuploader.com/product/uploader/json-upload-hoodie-heather.jpg',
       IMGSRC: 'http://productuploader.com/product/uploader/json-upload-hoodie-heather.jpg',
       Item: 'Hoodie',
@@ -105,62 +96,52 @@ var masterArray  = [ { 'Variant SKU': 'json-upload-hoodie-heather-sm',
 
 ]
 
-// got to iterate through array 
-// variants are each product
-// iterate through (starting at 1)
-
-// var shopifyProductObj = {};
+var shopifyProductObj = {}
 
 // first row (main product obj info)
-var  shopifyProductObj =  {
-    "product": {
-      // "id": 1071559644,
-      "title": masterArray[0]["Title"],
-      "body_html": masterArray[0]["Body (HTML)"],
-      "vendor": masterArray[0]["Vendor"],
-      "product_type": masterArray[0]["Type"],
-      // "created_at": "2016-04-25T17:00:06-04:00",
-      "handle": masterArray[0]["Handle"],
-      // "updated_at": "2016-04-25T17:00:06-04:00",
-      "published": masterArray[0]["Published"],
-      "template_suffix": null,
-      "published_scope": "global",
-      "tags": masterArray[0]["Tags"],
-      "variants": [],
-      "options": [
-        // COLOR
-        {
-          // "id": 1022828722,
-          // "product_id": 1071559644,
-          "name": masterArray[0]["Option1 Name"],
-          "position": 1,
-          "values": [
-            // colors
-            // masterArray[0]["Option1 Value"]
-          ]
-        },
-        // is this how you specify more options?
-        // size?
-        {
-          // "id": 1022828722,
-          // "product_id": 1071559644,
-          "name": masterArray[0]["Option2 Name"],
-          "position": 2,
-          "values": [
-            // colors
-            // masterArray[0]["Option2 Value"]
-          ]
+function createProduct (array) {
+  shopifyProductObj =  {
+      "product": {
+        // "id": 1071559644,
+        "title": array[0]["Title"],
+        "body_html": array[0]["Body (HTML)"],
+        "vendor": array[0]["Vendor"],
+        "product_type": array[0]["Type"],
+        "handle": array[0]["Handle"],
+        "published": array[0]["Published"],
+        "template_suffix": null,
+        "published_scope": "global",
+        "tags": array[0]["Tags"],
+        "variants": [],
+        "options": [
+          // COLOR
+          {
+            "name": array[0]["Option1 Name"],
+            "position": 1,
+            "values": [// array[i]["Option1 Value"]
+            ]
+          },
+          // size
+          {
+            "name": array[0]["Option2 Name"],
+            "position": 2,
+            "values": []
+          }
+        ],
+         "images" : {
+            "src": [array[0]["Image Src"]],
+            "variant_ids": []
         }
-      ]
+      }
     }
-  }
+}
+
 
 // product variants (each row / product obj)
 function addProductVariant (array) {
   for (i = 0; i < array.length; i++) {
-    // console.log("color", array[i]["Option1 Name"])
     var productVariant = {
-
+          "id": i,
           "handle": array[i]["Handle"],
           "price": array[i]["Variant Price"],
           "sku": array[i]["Variant SKU"],
@@ -178,22 +159,17 @@ function addProductVariant (array) {
           "barcode": null,
           "image_id": null,
           "inventory_quantity": 1,
-          // "weight": 0.0,
           "weight_unit": "oz",
           "old_inventory_quantity": 1,
           "requires_shipping": true
         }
     shopifyProductObj.product.variants.push(productVariant);
-      // ]
     }
   }
-
-
-  // FILTER OUT DUPLICATES!!
   // variant options (size and color)
   function addProductVariantOptions (array) {
     for (i = 0; i < array.length; i++) { 
-      // cannot have doubles :(
+      // cannot have doubles
       if (shopifyProductObj.product.options[0].values.indexOf(array[i]["Option1 Value"]) === -1) {
         shopifyProductObj.product.options[0].values.push(array[i]["Option1 Value"]);
       } 
@@ -202,27 +178,33 @@ function addProductVariant (array) {
     }
   }
 
+  function addImageSrc (array) {
+    for (i = 0; i < array.length; i++) {
+      shopifyProductObj.product.images.src.push(array[i]["Image Src"])
+      shopifyProductObj.product.images["variant_ids"].push(array[i]["Variant Image"])
+    }
+  }
 
+  createProduct(masterArray)
   addProductVariant(masterArray);
   addProductVariantOptions(masterArray);
-  // addProductVariantOptionSize(masterArray);
+  addImageSrc(masterArray)
 
-  console.log("base Obj POSTING TO SHOPIFY:", shopifyProductObj);
+
+  // console.log("base Obj POSTING TO SHOPIFY:", shopifyProductObj);
   console.log("VARIANT Obj POSTING TO SHOPIFY:", shopifyProductObj.product.variants);
-  console.log("OPTIONS Obj POSTING TO SHOPIFY:", shopifyProductObj.product.options);
+  console.log("VARIANT Obj POSTING TO SHOPIFY:", shopifyProductObj.product.images);
 
 uploadController = {
   postProduct: function (req, res) {
       console.log("POST REQ FNC HIT!")
       request({
       url: apiPostUrl, //URL to hit
-      // qs: {from: 'blog example', time: +new Date()}, //Query string data
       method: 'POST',
       headers: {
           'Content-Type': 'application/json',
-          // 'Custom-Header': 'Custom Value'
       },
-      body: JSON.stringify(shopifyProductObj) //Set the body product obj
+      body: JSON.stringify(shopifyProductObj) //stringify the body product obj
     }, function(error, response, body){
         if(error) {
             console.log(error);
@@ -235,17 +217,14 @@ uploadController = {
   getAllProducts: function (req, res) {
       request({
       url: apiPostUrl, //URL to hit
-      // qs: {from: 'blog example', time: +new Date()}, //Query string data
       method: 'GET',
       headers: {
           'Content-Type': 'application/json',
-          // 'Custom-Header': 'Custom Value'
       },
     }, function(error, response, body){
         if(error) {
             console.log(error);
         } else {
-            // console.log(response.statusCode, body);
             console.log("response:", response, "BODY", body);
             res.send(response)
         }
